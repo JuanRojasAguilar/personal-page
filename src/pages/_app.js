a3534333ee8710bb51f2d8374f352f4d5d3ea2d4
juan <yo217@hotmail.es>
2023-01-09 01:53:34 -0500

    font added and running

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,26 @@
 import MainLayout from '@layout/MainLayout';
+import localFont from '@next/font/local';
 import '@styles/global.css';
+
+const sofia = localFont({
+    src: [
+        {
+            path: '../fonts/SofiaSans-Italic-VariableFont_wght.ttf',
+            style: 'italic',
+        },
+        {
+            path: '../fonts/SofiaSans-VariableFont_wght.ttf',
+            style: 'normal',
+        }
+    ]
+})
 
 export default function MyApp({ Component, pageProps }) {
     return(
-        <>
+        <main className={sofia.className}>
             <MainLayout>
                 <Component {...pageProps} />
             </MainLayout>
-        </>
+        </main>
     )
 }
