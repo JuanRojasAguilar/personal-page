6ba0f9e599381e57aba7ce1b360451806d7f7334
juan <yo217@hotmail.es>
2023-01-09 02:33:07 -0500

    description and image added, work in progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Image from 'next/image';
+import tinyMe from '@icons/tinyMe.jpg'
 import Styles from '@styles/Home.module.scss';
 
 export default function Home() {
@@ -8,7 +10,22 @@ export default function Home() {
         <title>JuanDeveloper</title>
       </Head>
       <main className={Styles.mainHome}>
-        <h1>funciona?</h1>
+        <div className={Styles.presentation}>
+          <h1>Who am I?</h1>
+          <p>
+            I'm a colombian developer who likes to do easy code with excelent performance and maintainability. <br />
+            I consider myself a great team player with high autonomy so you can rely on me to do the work and also to understand the goals and flow of the team so you don't have to worry about things like communication, interpersonal relations or proactivity because those are my insignias. You got a solution that i can develop!
+          </p>
+        </div>
+        <picture className={Styles.picture}>
+          <Image
+            src={tinyMe}
+            // fill
+            alt="A photo of me"
+            className={Styles.photo}
+            sizes='30vw, 35vh'
+            />
+        </picture>
       </main>
     </>
   )
